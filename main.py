8073cdf61cd8437d6630b46dc53bb52292a65208
Hugo Mello <hugo.mello8@gmail.com>
2026-01-24 02:13:27 -0300

    add origin on top 1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from routers.supabase_tables import router
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:5173', 'https://stately-treacle-e466de.netlify.app/'],
+    allow_origins=['http://localhost:5173', 'https://stately-treacle-e466de.netlify.app'],
     allow_credentials=True,
     allow_headers=['*'],
     allow_methods=['*'])
