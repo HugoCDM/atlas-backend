362b8778ba6f7e96bc041663c44527b599ecbee7
Hugo Mello <hugo.mello8@gmail.com>
2026-01-27 18:19:33 -0300

    new router added v2

--- routers/supabase_tables.py
+++ routers/supabase_tables.py
@@ -192,17 +192,17 @@ async def get_all_data():
             return supabase_client.table(table_name, columns).execute()
     
         data = {
-            'train_stations': fetch_table('estacoes-trem, nome, rua, presencaRamais, latitude, longitude').data,
-            'metro_stations': fetch_table('estacoes-metro, nome, latitude, longitude').data,
-            'federal_schools': fetch_table('escolas-federais, unidade, endereco, zona, telefone latitude, longitude').data,
-            'state_schools': fetch_table('escolas-estaduais, unidade, endereco, zona, telefone, latitude, longitude').data,
-            'municipal_schools': fetch_table('escolas-municipais, nome, tipo, latitude, longitude').data,
-            'squares': fetch_table('pracas, nomeCompleto, endereco, ap, latitude, longitude').data,
-            'hospitals': fetch_table('unidades-saude-municipais, NOME, ENDERECO, BAIRRO, TIPO_data, CNES, HORARIO_SEMANA, TELEFONE, latitude, longitude').data,
-            'equipments': fetch_table('gestao-equipamento-smas2023, nome_equip, endereco, bairro, bairros_at, hierarquia, telefone, latitude, longitude').data,
-            'vlt': fetch_table('vlt-paradas,nome, latitude, longitude').data,
-            'brt': fetch_table('estacoes-brt,nome, corredor, latitude, longitude').data,
-            'supermarkets': fetch_table('supermercados, nome, latitude, longitude').data,
+            'train_stations': fetch_table('estacoes-trem', 'nome, rua, presencaRamais, latitude, longitude').data,
+            'metro_stations': fetch_table('estacoes-metro', 'nome, latitude, longitude').data,
+            'federal_schools': fetch_table('escolas-federais', 'unidade, endereco, zona, telefone latitude, longitude').data,
+            'state_schools': fetch_table('escolas-estaduais', 'unidade, endereco, zona, telefone, latitude, longitude').data,
+            'municipal_schools': fetch_table('escolas-municipais', ' nome, tipo, latitude, longitude').data,
+            'squares': fetch_table('pracas', ' nomeCompleto, endereco, ap, latitude, longitude').data,
+            'hospitals': fetch_table('unidades-saude-municipais', ' NOME, ENDERECO, BAIRRO, TIPO_data, CNES, HORARIO_SEMANA, TELEFONE, latitude, longitude').data,
+            'equipments': fetch_table('gestao-equipamento-smas2023', ' nome_equip, endereco, bairro, bairros_at, hierarquia, telefone, latitude, longitude').data,
+            'vlt': fetch_table('vlt-paradas', 'nome, latitude, longitude').data,
+            'brt': fetch_table('estacoes-brt', 'nome, corredor, latitude, longitude').data,
+            'supermarkets': fetch_table('supermercados', ' nome, latitude, longitude').data,
         }
 
         return {
