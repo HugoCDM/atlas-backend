2a7986b9d153d6bb869ee91a08a84ef8f668990f
Hugo Mello <51499377+HugoCDM@users.noreply.github.com>
2026-04-13 11:04:58 -0300

    Update CORS origins to include additional localhost ports

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ from routers.supabase_tables import router
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:5173', 'https://stately-treacle-e466de.netlify.app'],
+    allow_origins=[
+        'http://localhost:5173',
+        'http://localhost:5500',
+        'http://127.0.0.1:5500',
+        'http://localhost:3000',
+        'https://stately-treacle-e466de.netlify.app',
+    ],
     allow_credentials=True,
     allow_headers=['*'],
     allow_methods=['*'])
